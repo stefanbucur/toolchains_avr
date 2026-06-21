fe22d3d19a395450a6967d9fade9e67ab3fa889d
Stefan Bucur <stefan.bucur@gmail.com>
2026-06-21 16:44:26 -0400

    examples/blinkie-rs: add idle sleep in hello firmware

--- examples/blinkie-rs/firmware/hello/src/main.rs
+++ examples/blinkie-rs/firmware/hello/src/main.rs
@@ -6,9 +6,17 @@ use core::panic::PanicInfo;
 #[avr_device::entry]
 fn main() -> ! {
     let dp = avr_device::avr128db48::Peripherals::take().unwrap();
+
     dp.PORTB.dirset().write(|w| w.pb3().set_bit());
     dp.PORTB.outclr().write(|w| w.pb3().set_bit());
-    loop {}
+
+    dp.SLPCTRL.ctrla().write(|w| {
+        w.smode().idle();
+        w.sen().set_bit()
+    });
+    loop {
+        avr_device::asm::sleep();
+    }
 }
 
 #[panic_handler]
